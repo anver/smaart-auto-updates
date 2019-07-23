3f50ccec1f9c7819b3a491b59859b7f5449dc5c2
anver <anvergdr@gmail.com>
2019-07-23 20:37:41 +0530

    Apply WordPress formatting standards

--- src/Locator.php
+++ src/Locator.php
@@ -2,15 +2,20 @@
 
 namespace SmaartWeb\AutoUpdates;
 
+/**
+ * Locator class
+ */
 class Locator {
 
-    /**
-     * Gets the absolute path from the 
-     * @access public
-     */
-    public function get_plugins_absolute_path( $plugin_path ) {
-        //return trailingslashit( substr_replace( __FILE__, PLUGINDIR, strpos( __FILE__, PLUGINDIR ) ) ) . $plugin_path;
-        return WP_PLUGIN_DIR . '/' . $plugin_path;
-    }
+	/**
+	 * Gets the absolute path from the
+	 *
+	 * @param string $plugin_path The plugin path.
+	 * @access public
+	 */
+	public function get_plugins_absolute_path( $plugin_path ) {
+		// return trailingslashit( substr_replace( __FILE__, PLUGINDIR, strpos( __FILE__, PLUGINDIR ) ) ) . $plugin_path;
+		return WP_PLUGIN_DIR . '/' . $plugin_path;
+	}
 
 }
